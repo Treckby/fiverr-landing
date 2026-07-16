3fda8839c5e09daafc4691910376a667bde6a5c0
el_55 <jesrodriguezalv@gmail.com>
2026-07-16 16:47:31 -0700

    creando container

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import Container from "./components/Container";
+
 export default function Home() {
   return (
     <main>
-      Hola Mundo.
+      <Container>
+        <h1 className="py-20 text-5xl font-bold">
+          Fiverr Landing
+        </h1>
+      </Container>
     </main>
   );
 }
